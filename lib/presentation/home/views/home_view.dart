b882ad85dc201a736a88e25090b1a534510120c2
A B O U D <aboudkalaf10@gmail.com>
2025-05-26 17:45:01 +0300

    search bar widget done ✅

--- lib/presentation/home/views/home_view.dart
+++ lib/presentation/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:bac_project/core/widgets/ui/search_bar_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +6,19 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Home View")));
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home View")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SearchBarWidget(
+          onChanged: (value) {
+            // Handle search text changes
+          },
+          onFieldSubmitted: (value) {
+            // Handle search submission
+          },
+        ),
+      ),
+    );
   }
 }
