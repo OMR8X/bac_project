e6867c3faf771554e985e2efb3c2bef54183451e
Mayar911 <mayartaky23@gmail.com>
2025-06-02 09:28:19 +0200

    Task 2 mayar

--- lib/presentation/home/views/home_view.dart
+++ lib/presentation/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:bac_project/core/widgets/ui/search_bar_widget.dart';
+import 'package:bac_project/presentation/home/widgets/home_card_bilder_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -8,16 +9,26 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Home View")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SearchBarWidget(
-          onChanged: (value) {
-            // Handle search text changes
-          },
-          onFieldSubmitted: (value) {
-            // Handle search submission
-          },
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SearchBarWidget(
+              onChanged: (value) {
+                // Handle search text changes
+              },
+              onFieldSubmitted: (value) {
+                // Handle search submission
+              },
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 8.0, left: 8),
+              child: HomeCardsBuilderWidget(),
+            ),
+          ),
+        ],
       ),
     );
   }
