5bc46585926be9b812429dd710bac4918afe690e
omr8x <omar.khamis.attallah@gmail.com>
2025-06-11 17:15:37 +0300

    fixes

--- lib/presentation/home/views/home_view.dart
+++ lib/presentation/home/views/home_view.dart
@@ -1,9 +1,7 @@
-
 import 'package:bac_project/core/injector/app_injection.dart';
 import 'package:bac_project/core/services/localization/localization_keys.dart';
 import 'package:bac_project/core/services/localization/localization_manager.dart';
 import 'package:bac_project/core/widgets/ui/custom_action_card_widget.dart';
-
 import 'package:bac_project/core/widgets/ui/search_bar_widget.dart';
 import 'package:bac_project/presentation/home/widgets/home_action_card_bilder_widget.dart';
 import 'package:flutter/material.dart';
@@ -16,21 +14,25 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(LocalizationManager().get(LocalizationKeys.home.title))),
-      body: Padding(
-        padding: PaddingResources.screenSidesPadding,
-        child: Column(
-          children: [
-            SearchBarWidget(
-              onChanged: (value) {
-                // Handle search text changes
-              },
-              onFieldSubmitted: (value) {
-                // Handle search submission
-              },
-            ),
-            Expanded(child: HomeCardsBuilderWidget()),
-          ],
+      body: NestedScrollView(
+        headerSliverBuilder:
+            (context, innerBoxIsScrolled) => [
+              SliverAppBar(
+                floating: true,
+                snap: true,
+                title: Text(LocalizationManager().get(LocalizationKeys.home.title)),
+                forceElevated: innerBoxIsScrolled,
+              ),
+            ],
+        body: Padding(
+          padding: PaddingResources.screenSidesPadding,
+          child: Column(
+            children: [
+              SearchBarWidget(onChanged: (value) {}, onFieldSubmitted: (value) {}),
+              const SizedBox(height: 10),
+              Expanded(child: HomeCardsBuilderWidget()),
+            ],
+          ),
         ),
       ),
     );
